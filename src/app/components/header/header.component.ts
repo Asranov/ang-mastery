9e1576547f2adb9e85f3031fb28172e944bd7fd2
Asranov <asranov0003@gmail.com>
2023-07-27 12:36:24 +0500

    send args

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+  @Input() title: string = "Welcome to header";
+  color = "red"
 
+  foo() {
+    console.log(1)
+  }
 }
