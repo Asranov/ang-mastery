2c2c27eca1df3682ca3b375aec773a8db2321361
Asranov <asranov0003@gmail.com>
2023-07-27 11:31:22 +0500

    variables

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-tutorial';
+  names: string[] = ["Stiv", "Alice", "Bob"]
+  newNames: string[] = []
+  numbers: number[] = [1, 2, 3, 4, 5]
+  filterdNumbers: number[] = []
+
+  constructor() {
+    this.filterdNumbers = this.numbers.filter(n => n % 2 === 0)
+
+    this.names.forEach((name) => this.newNames.push(name))
+
+    console.log("New names: ", this.newNames)
+
+    console.log(this.filterdNumbers)
+  }
+
 }
