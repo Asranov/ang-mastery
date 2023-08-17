2e112854b0b18c4acc73ad97bdd6161ba9b39e92
Asranov <asranov0003@gmail.com>
2023-08-17 12:50:32 +0500

    users info

--- src/app/components/messangers/messangers.component.ts
+++ src/app/components/messangers/messangers.component.ts
@@ -8,26 +8,62 @@ interface IMessangers {
   openedYear: number;
 }
 
+interface IUser {
+  id: number;
+  name: string;
+  age: number;
+}
+
 @Component({
   selector: 'app-messangers',
   templateUrl: './messangers.component.html',
   styleUrls: ['./messangers.component.css']
 })
 export class MessangersComponent implements OnInit {
-  selectedMessenger: any;
+  selectedUser: IUser | undefined;
+  selectedMessenger: IMessangers | undefined;
   messengers: IMessangers[] = [
     { id: 'telegram', name: 'Telegram', ownerName: 'Pavel Durov', title: 'Messaging App', openedYear: 2013 },
     { id: 'instagram', name: 'Instagram', ownerName: 'Kevin Systrom', title: 'Photo Sharing App', openedYear: 2010 },
     { id: 'facebook', name: 'Facebook', ownerName: 'Mark Zuckerberg', title: 'Social Media Platform', openedYear: 2004 },
     { id: 'twitter', name: 'Twitter', ownerName: 'Jack Dorsey', title: 'Microblogging Platform', openedYear: 2006 }
   ];
+  selectedUserExpanded: boolean = false;
+
+  users: IUser[] = [
+    {
+      id: 1,
+      name: "John",
+      age: 20
+    },
+    {
+      id: 2,
+      name: "Stiv",
+      age: 21
+    },
+    {
+      id: 3,
+      name: "Alice",
+      age: 56
+    }
+  ]
+
+  onUserClick(user: IUser) {
+    if (this.selectedUser === user) {
+      this.selectedUserExpanded = !this.selectedUserExpanded;
+    } else {
+      this.selectedUser = user;
+      this.selectedUserExpanded = false;
+    }
+  }
+
 
   ngOnInit() {
     this.selectedMessenger = this.messengers.find(messenger => messenger.id === 'telegram');
   }
 
   onMessengerChange() {
-    const selected = this.messengers.find(messenger => messenger.id === this.selectedMessenger);
+    const selected = this.messengers.find(messenger => messenger.id === (this.selectedMessenger ? this.selectedMessenger.id : undefined));
 
     if (selected) {
       this.selectedMessenger = selected;
